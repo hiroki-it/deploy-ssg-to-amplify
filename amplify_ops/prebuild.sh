8fbd4fcf7ee80fbfde721aae8431a7ca8e19cdb7
hiroki-it <hasegawafeedshop@gmail.com>
2021-05-23 19:42:15 +0900

    update 行間を揃えないようにした．

--- amplify_ops/prebuild.sh
+++ amplify_ops/prebuild.sh
@@ -5,10 +5,10 @@ yarn install
 ###########################################################
 # envファイルを作成します．
 ###########################################################
-echo "API_BASE_URL=$API_BASE_URL"                  > .env
+echo "API_BASE_URL=$API_BASE_URL" > .env
 echo "API_BROWSER_BASE_URL=$API_BROWSER_BASE_URL" >> .env
-echo "CLIENT_SECRET=$CLIENT_SECRET"               >> .env
-echo "GOOGLE_MAPS_API_KEY=$GOOGLE_MAPS_API_KEY"   >> .env
-echo "GOOGLE_ANALYTICS_ID=$GOOGLE_ANALYTICS_ID"   >> .env
-echo "HTTPS_PORTAL_DOMAIN=$HTTPS_PORTAL_DOMAIN"   >> .env
+echo "CLIENT_SECRET=$CLIENT_SECRET" >> .env
+echo "GOOGLE_MAPS_API_KEY=$GOOGLE_MAPS_API_KEY" >> .env
+echo "GOOGLE_ANALYTICS_ID=$GOOGLE_ANALYTICS_ID" >> .env
+echo "HTTPS_PORTAL_DOMAIN=$HTTPS_PORTAL_DOMAIN" >> .env
 cat .env
